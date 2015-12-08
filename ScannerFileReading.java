b9a444af8a6c42bcf82b860c1ad4993b667f39e6
Murat Çakmak <mrtstab@gmail.com>
2015-12-08 21:48:14 +0200

    Create ScannerFileReading.java

---
+++ ScannerFileReading.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+import java.io.*;
+public class ScannerFileReading {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		File date = new File("file.txt");
+		Scanner file = new Scanner(date);
+		
+		while(file.hasNext()){
+			System.out.println(file.nextLine());
+		}
+	}
+}
